a0be1547475891f3b511bd647e5aeaec640a7214
Douglimar Moraes <douglimar@gmail.com>
2018-07-28 23:45:43 -0300

    Commit over Linux -- 2018.07.28

--- app/src/main/java/br/com/douglimar/surpresinha_lotofacil/MainActivity.java
+++ app/src/main/java/br/com/douglimar/surpresinha_lotofacil/MainActivity.java
@@ -1,11 +1,16 @@
 package br.com.douglimar.surpresinha_lotofacil;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         Button btnJogoUnico = findViewById(R.id.btnSingleGame);
         Button btnJogosMultiplos = findViewById(R.id.btnMultipleGames);
         Button btnLastResults = findViewById(R.id.btnLastResults);
+        Button btnGooglePlay = findViewById(R.id.btnGooglePlay);
 
         final Surpresinha surpresinha = new Surpresinha();
         final Intent intent = getIntent();
@@ -81,7 +87,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                carregaWebView2();
+                if (isNetworkAvailable())
+                    carregaWebView2();
+                else
+                    Toast.makeText(getApplicationContext(), R.string.internet_conn, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        btnGooglePlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isNetworkAvailable())
+                    openGooglePlay();
+                else
+                    Toast.makeText(getApplicationContext(), R.string.internet_conn, Toast.LENGTH_LONG).show();
 
             }
         });
@@ -116,6 +137,30 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
 
+    }
+
+    private void openGooglePlay() {
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "OpenGooglePlay");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "GooglePlay");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
+        final String appPackageName = "ddmsoftware"; // getPackageName() from Context or Activity object
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/search?q=" + appPackageName)));
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert connectivityManager != null;
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 }
